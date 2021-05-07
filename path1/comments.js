be1f7fa9014038b0fba0c0f7ea7ef5ba09d067a1
GtlsGamr <hittarth91@gmail.com>
2021-05-07 21:00:03 +0530

    pseudo-code for the json path

---
+++ path1/comments.js
@@ -0,0 +1,8 @@
+$.getJSON('https://jsonplaceholder.typicode.com/comments', function(data) {
+	//post id = get post id from url
+	// run loop in json to append text to the comments div
+                    
+        d1.insertAdjacentHTML('afterend', '<div id="two">two</div>');
+
+    });
+
